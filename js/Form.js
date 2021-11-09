e41cc69c63e1b8d5bdbd3077ce93f32a31174ae7
EuSouJJ <89562232+EuSouJJ@users.noreply.github.com>
2021-11-10 00:18:22 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -8,15 +8,19 @@ class Form {
 
     //cria elementos html (botão, entrada e texto)
  
-    var input = createInput("");
+    var input = createInput("",'required');
   
     document.getElementsByTagName("INPUT")[0].placeholder = "Name";
-    document.getElementsByTagName("INPUT").id = "FormName";
+    document.getElementsByTagName("INPUT")[0].id = "FormName";
+    console.log(input);
     
 
     var button = createButton("Jogar");
     var greeting = createElement("h3");
-
+    var nomePorfavor = createElement("h4");
+    
+    
+  
     //ajeita posição dos elementos html
     //input.position(130, 160);
     //button.position(200, 220);
@@ -28,14 +32,26 @@ class Form {
       button.hide();
 
       var nome = input.value();
+      //presentedBy.html("Presented By Bia &#128151; JJ")
 
       //aumentar o valor do numJogadores e atualizar o nome do jogador no banco de dados
-      numJogadores += 1;
-      jogador.update(nome);
-      jogador.updateCount(numJogadores);
+     
+  
 
       //mostrar mensagem de boas-vindas
-      greeting.html("Olá " + nome);
+      if(nome)
+      {
+          numJogadores += 1;
+          jogador.update(nome);
+          jogador.updateCount(numJogadores);
+          greeting.html("Olá " + nome +" &#128151;");
+        
+      }
+      else
+      {
+        nomePorfavor.html("Por favor, insira seu nome para continuar &#128512;");
+      }
+      
       //greeting.position(540, 250);
     });
   }
